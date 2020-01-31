ebcc5a5a04a7c93773ab9bf750e76d16c7be1659
Victor Oomens <victor@oomens-ict.nl>
2020-01-31 16:58:41 +0100

    Added duotone pro fonts

--- FontAwesome/FontAwesome.swift
+++ FontAwesome/FontAwesome.swift
@@ -47,6 +47,7 @@ public enum FontAwesomeStyle: String {
     case light
     case regular
     case brands
+    case duotone
 
     func fontName() -> String {
         switch self {
@@ -56,6 +57,8 @@ public enum FontAwesomeStyle: String {
             return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Light" : "FontAwesome5Free-Regular"
         case .regular:
             return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Regular" : "FontAwesome5Free-Regular"
+        case .duotone:
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Duotone" : "FontAwesome5Free-Duotone"
         case .brands:
             return "FontAwesome5Brands-Regular"
         }
@@ -69,6 +72,8 @@ public enum FontAwesomeStyle: String {
             return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro-Light-300" : "Font Awesome 5 Free-Regular-400"
         case .regular:
             return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro-Regular-400" : "Font Awesome 5 Free-Regular-400"
+        case .duotone:
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Duotone-Solid-900" : "Font Awesome 5 Free-Regular-400"
         case .brands:
             return "Font Awesome 5 Brands-Regular-400"
         }
@@ -80,6 +85,7 @@ public enum FontAwesomeStyle: String {
             return "Font Awesome 5 Brands"
         case .regular,
              .light,
+             .duotone,
              .solid:
             return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro" : "Font Awesome 5 Free"
         }
