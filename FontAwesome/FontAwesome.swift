f38000c0c9df8782b4d3d64afbdf9689c4b6ff47
Victor Oomens <victor@oomens-ict.nl>
2024-04-10 15:21:20 +0200

    Added pro 6 fonts

--- FontAwesome/FontAwesome.swift
+++ FontAwesome/FontAwesome.swift
@@ -52,42 +52,41 @@ public enum FontAwesomeStyle: String {
     func fontName() -> String {
         switch self {
         case .solid:
-            return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Solid" : "FontAwesome5Free-Solid"
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome6Pro-Solid" : "FontAwesome5Free-Solid"
         case .light:
-            return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Light" : "FontAwesome5Free-Regular"
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome6Pro-Light" : "FontAwesome5Free-Regular"
         case .regular:
-            return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Regular" : "FontAwesome5Free-Regular"
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome6Pro-Regular" : "FontAwesome5Free-Regular"
         case .duotone:
-            return FontAwesomeConfig.usesProFonts ? "FontAwesome5Pro-Duotone" : "FontAwesome5Free-Duotone"
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome6Pro-Duotone" : "FontAwesome5Free-Duotone"
         case .brands:
-            return "FontAwesome5Brands-Regular"
+            return FontAwesomeConfig.usesProFonts ? "FontAwesome6Brands-Regular" : "FontAwesome5Brands-Regular"
         }
     }
 
     func fontFilename() -> String {
         switch self {
         case .solid:
-            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro-Solid-900" : "Font Awesome 5 Free-Solid-900"
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Pro-Solid-900" : "Font Awesome 5 Free-Solid-900"
         case .light:
-            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro-Light-300" : "Font Awesome 5 Free-Regular-400"
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Pro-Light-300" : "Font Awesome 5 Free-Regular-400"
         case .regular:
-            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro-Regular-400" : "Font Awesome 5 Free-Regular-400"
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Pro-Regular-400" : "Font Awesome 5 Free-Regular-400"
         case .duotone:
-            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Duotone-Solid-900" : "Font Awesome 5 Free-Regular-400"
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Duotone-Solid-900" : "Font Awesome 5 Free-Regular-400"
         case .brands:
-            return "Font Awesome 5 Brands-Regular-400"
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Brands-Regular-400" : "Font Awesome 5 Brands-Regular-400"
         }
     }
 
     func fontFamilyName() -> String {
         switch self {
-        case .brands:
-            return "Font Awesome 5 Brands"
         case .regular,
              .light,
              .duotone,
-             .solid:
-            return FontAwesomeConfig.usesProFonts ? "Font Awesome 5 Pro" : "Font Awesome 5 Free"
+             .solid,
+             .brands:
+            return FontAwesomeConfig.usesProFonts ? "Font Awesome 6 Pro" : "Font Awesome 5 Free"
         }
     }
 }
